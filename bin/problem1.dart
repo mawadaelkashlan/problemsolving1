e4a1f540bcc974150eda7eaf1b921e999d6a5b4f
Mawada Elkashlan <mawadaelkashlan@gmail.com>
2024-07-23 23:39:10 +0300

    problem solving 1

---
+++ bin/problem1.dart
@@ -0,0 +1,22 @@
+void main() {
+  List<String> strs = ["flower", "flow", "flight"];
+  print(longestCommonPrefix(strs));
+}
+
+String longestCommonPrefix(List<String> strs) {
+  if (strs.isEmpty) {
+    return "";
+  }
+  String prefix = strs[0];
+
+  for (int i = 1; i < strs.length; i++) {
+    while (!strs[i].startsWith(prefix)) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix.isEmpty) {
+        return "";
+      }
+    }
+  }
+
+  return prefix;
+}
